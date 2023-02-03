dee21bce8d2d65c91a1d92eeed0e46002f60da18
Timothy Nguyen <timhnguyen98@gmail.com>
2023-02-03 16:58:32 +1100

    fixed nav bar and landing

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
+import { Container, Row, Col, Button } from 'react-bootstrap'
 
 const LandingPage = () => {
   return (
-    <div className="container">
-      <h1>Welcome to the Job Seeking Website</h1>
-      <p>Find your next career opportunity here</p>
-      <div>
-        <a href="/register">Register</a>
-        <a href="/login">Login</a>
-      </div>
+    <div style={{ backgroundColor: '#D9E8F5', height: '100vh' }}>
+      <Container style={{ paddingTop: '10%' }}>
+        <Row>
+          <Col className="text-center">
+            <h1 style={{ fontWeight: 'bold', color: '#495057' }}>Welcome to Steve's Jobs</h1>
+            <p style={{ fontSize: '1.2rem', color: '#495057' }}>
+              Find your dream job or post a job listing and start your journey today.
+            </p>
+            <p>
+              <Button variant="outline-primary" href='/jobs/'>View Jobs</Button>
+            </p>
+          </Col>
+        </Row>
+      </Container>
     </div>
   )
 }
