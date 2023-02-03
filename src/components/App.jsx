a27ff4a504af7357a461d843159018186500e34b
matthewngc <13530@coderacademy.edu.au>
2023-02-04 01:18:22 +1100

    Linked job posting page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useParams } from 'react-router-dom'
 import EmployerDashboard from './EmployerDashboard'
 import JobListingsPage from './JobListingsPage'
 import JobPostingPage from './JobPostingPage'
@@ -22,20 +22,27 @@ const App = () => {
     async function getListings() {
       const res = await fetch('http://localhost:4002/jobs')
       const data = await res.json()
-      console.log(data)
       setJobListings(data)
     }
     getListings()
   }, [])
 
+  // Higher Order Components
+  const JobPostingPageWrapper = () => {
+    const { id } = useParams()
+    const listing = jobListings.find(listing => listing._id == id)
+    console.log(jobListings)
+    console.log(listing)
+    return listing ? <JobPostingPage listing={listing} /> : <h4>Job Listing not found!</h4>
+  }
 
   return (
-    <BrowserRouter>
+    <>
       <NavBar />
       <Routes>
         <Route path='/' element={<LandingPage />} />
         <Route path='/jobs' element={<JobListingsPage jobListings={jobListings}/>} />
-        <Route path='/jobs/:id' element={<JobPostingPage />} />
+        <Route path='/jobs/:id' element={<JobPostingPageWrapper />} />
         <Route path='/employer-dashboard' element={<EmployerDashboard />} />
         <Route path='/job-seeker-dashboard' element={<JobSeekerDashboard />} />
         <Route path='/login' element={<LoginPage />} />
@@ -47,7 +54,7 @@ const App = () => {
         <Route path='/contact' element ={<ContactUs />} />
       </Routes>
       <Footer />
-    </BrowserRouter>
+    </>
   )
 }
 
