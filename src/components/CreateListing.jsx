49431b973d72d86dce42fd4ec3b3f585524b752d
antohuynh <anthony.huynh97@hotmail.com>
2023-02-03 21:52:50 +1100

    created listing form

--- src/components/CreateListing.jsx
+++ src/components/CreateListing.jsx
@@ -14,11 +14,49 @@ const CreateListing = ({setListings, listings}) => {
   return (
     <>
     <h2>Create New Listing</h2>
-    <form>
-        <div>
-            <textarea></textarea>
-        </div>
-    </form>
+    <form class="row g-3">
+  <div class="col-md-4">
+    <label for="validationDefault01" class="form-label">Job Title</label>
+    <input type="text" class="form-control" id="validationDefault01" value="Mark" required></input>
+  </div>
+  <div class="col-md-4">
+    <label for="validationDefault02" class="form-label">Company</label>
+    <input type="text" class="form-control" id="validationDefault02" value="Otto" required></input>
+  </div>
+  <div class="col-md-4">
+    <label for="validationDefaultUsername" class="form-label">Contact Email</label>
+    <div class="input-group">
+      <span class="input-group-text" id="inputGroupPrepend2">@</span>
+      <input type="text" class="form-control" id="validationDefaultUsername"  aria-describedby="inputGroupPrepend2" required></input>
+    </div>
+  </div>
+  <div class="col-md-6">
+    <label for="validationDefault03" class="form-label">City</label>
+    <input type="text" class="form-control" id="validationDefault03" required></input>
+  </div>
+  <div class="col-md-3">
+    <label for="validationDefault04" class="form-label">State</label>
+    <select class="form-select" id="validationDefault04" required>
+      <option selected disabled value="">Choose...</option>
+      <option>...</option>
+    </select>
+  </div>
+  <div class="col-md-3">
+    <label for="validationDefault05" class="form-label">Postcode</label>
+    <input type="text" class="form-control" id="validationDefault05" required></input>
+  </div>
+  <div class="col-12">
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" value="" id="invalidCheck2" required></input>
+      <label class="form-check-label" for="invalidCheck2">
+        Agree to terms and conditions
+      </label>
+    </div>
+  </div>
+  <div class="col-12">
+    <button class="btn btn-primary" type="submit">Submit form</button>
+  </div>
+</form>
     </>
   )
 }
