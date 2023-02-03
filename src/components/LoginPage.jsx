c81ef7666fb67bcca058c61c45f0d46386963aa9
matthewngc <13530@coderacademy.edu.au>
2023-02-04 06:17:56 +1100

    Fixed login & made it functional

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,103 +1,118 @@
 import React, { useState } from "react"
 
-export default function (props) {
-  let [authMode, setAuthMode] = useState("signin")
+const Login = ({ userLogin }) => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
 
-  const changeAuthMode = () => {
-    setAuthMode(authMode === "signin" ? "signup" : "signin")
-  }
+    function submit(evt) {
+        evt.preventDefault()
+        userLogin(email, password)
+    }
 
-  if (authMode === "signin") {
+    const [authMode, setAuthMode] = useState("signin")
+
+    const changeAuthMode = () => {
+        setAuthMode(authMode === "signin" ? "signup" : "signin")
+    }
+    console.log(authMode)
+    if (authMode === "signin") {
+        return (
+        <div className="Auth-form-container">
+            <form onSubmit={submit} className="Auth-form">
+            <div className="Auth-form-content">
+                <h3 className="Auth-form-title">Sign In</h3>
+                <div className="text-center">
+                Not registered yet?{" "}
+                <span className="link-primary" onClick={changeAuthMode}>
+                    Sign Up
+                </span>
+                </div>
+                <div className="form-group mt-3">
+                <label>Email address</label>
+                <input
+                    type="email"
+                    value={email}
+                    onChange={(event) => setEmail(event.target.value)}
+                    className="form-control mt-1"
+                    placeholder="Enter email"
+                />
+                </div>
+                <div className="form-group mt-3">
+                <label>Password</label>
+                <input
+                    type="password"
+                    value={password}
+                    onChange={(event) => setPassword(event.target.value)}
+                    className="form-control mt-1"
+                    placeholder="Enter password"
+                />
+                </div>
+                <div className="d-grid gap-2 mt-3">
+                <button type="submit" className="btn btn-primary">
+                    Submit
+                </button>
+                </div>
+                <p className="text-center mt-2">
+                Forgot <a href="#">password?</a>
+                </p>
+            </div>
+            </form>
+        </div>
+        )
+        }
+
+    console.log(authMode)
+
     return (
-      <div className="Auth-form-container">
+        <div className="Auth-form-container">
         <form className="Auth-form">
-          <div className="Auth-form-content">
+            <div className="Auth-form-content">
             <h3 className="Auth-form-title">Sign In</h3>
             <div className="text-center">
-              Not registered yet?{" "}
-              <span className="link-primary" onClick={changeAuthMode}>
-                Sign Up
-              </span>
+                Already registered?{" "}
+                <span className="link-primary" onClick={changeAuthMode}>
+                Sign In
+                </span>
             </div>
             <div className="form-group mt-3">
-              <label>Email address</label>
-              <input
+                <label>Full Name</label>
+                <input
                 type="email"
                 className="form-control mt-1"
-                placeholder="Enter email"
-              />
+                placeholder="e.g Jane Doe"
+                />
             </div>
             <div className="form-group mt-3">
-              <label>Password</label>
-              <input
+                <label>Email address</label>
+                <input
+                type="email"
+                className="form-control mt-1"
+                placeholder="Email Address"
+                />
+            </div>
+            <div className="form-group mt-3">
+                <label>Password</label>
+                <input
                 type="password"
                 className="form-control mt-1"
-                placeholder="Enter password"
-              />
+                placeholder="Password"
+                />
             </div>
             <div className="d-grid gap-2 mt-3">
-              <button type="submit" className="btn btn-primary">
+                <button type="submit" className="btn btn-primary">
                 Submit
-              </button>
+                </button>
             </div>
             <p className="text-center mt-2">
-              Forgot <a href="#">password?</a>
+                Forgot <a href="#">password?</a>
             </p>
-          </div>
+            </div>
         </form>
-      </div>
-    )
-  }
-
-  return (
-    <div className="Auth-form-container">
-      <form className="Auth-form">
-        <div className="Auth-form-content">
-          <h3 className="Auth-form-title">Sign In</h3>
-          <div className="text-center">
-            Already registered?{" "}
-            <span className="link-primary" onClick={changeAuthMode}>
-              Sign In
-            </span>
-          </div>
-          <div className="form-group mt-3">
-            <label>Full Name</label>
-            <input
-              type="email"
-              className="form-control mt-1"
-              placeholder="e.g Jane Doe"
-            />
-          </div>
-          <div className="form-group mt-3">
-            <label>Email address</label>
-            <input
-              type="email"
-              className="form-control mt-1"
-              placeholder="Email Address"
-            />
-          </div>
-          <div className="form-group mt-3">
-            <label>Password</label>
-            <input
-              type="password"
-              className="form-control mt-1"
-              placeholder="Password"
-            />
-          </div>
-          <div className="d-grid gap-2 mt-3">
-            <button type="submit" className="btn btn-primary">
-              Submit
-            </button>
-          </div>
-          <p className="text-center mt-2">
-            Forgot <a href="#">password?</a>
-          </p>
         </div>
-      </form>
-    </div>
-  )
+    )
 }
 
+export default Login
 //   const history = useHistory();
 //   const [email, setEmail] = useState('');
 //   const [password, setPassword] = useState('');
