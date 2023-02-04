54c966ddb6e80a7d3b18a4becdba977282011d02
Timothy Nguyen <timhnguyen98@gmail.com>
2023-02-04 16:30:58 +1100

    did some navbar styling; removed search

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,61 +1,25 @@
 import React from 'react'
-// import { Nav, Navbar } from 'react-bootstrap'
+import './styles/NavBar.css'
 import { Link } from 'react-router-dom'
-// const NavBar = () => {
-//   return (
-//     <Navbar bg="light" expand="lg">
-//       <Navbar.Brand href="/">Steve's Jobs</Navbar.Brand>
-//       <Navbar.Toggle aria-controls="basic-navbar-nav" />
-//       <Navbar.Collapse id="basic-navbar-nav">
-//         <Nav className="mr-auto">
-//           <Nav.Link href="/dashboard">Dashboard</Nav.Link>
-//           <Nav.Link href="/jobs">Jobs</Nav.Link>
-//           <Nav.Link href="/login">Login</Nav.Link>
-//         </Nav>
-//       </Navbar.Collapse>
-//     </Navbar>
-//   )
-// }
 
 const NavBar = () => {
   return (
-
-<nav className="navbar navbar-expand-lg bg-body-tertiary">
-<div className="container-fluid">
-  <Link className="navbar-brand" to="/">Steve's Jobs</Link>
-  <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
-  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-      <li className="nav-item">
-        <Link className="nav-link active" aria-current="page" to="/dashboard">Dashboard</Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link active" to="/jobs">Jobs</Link>
-      </li>
-      {/* <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-          Dropdown
-        </a>
-        <ul class="dropdown-menu">
-          <li><a class="dropdown-item" href="#">Action</a></li>
-          <li><a class="dropdown-item" href="#">Another action</a></li>
-          <li><hr class="dropdown-divider"/></li>
-          <li><a class="dropdown-item" href="#">Something else here</a></li>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid d-flex justify-content-between">
+        <Link className="navbar-brand text-white h3 font-weight-bold" to="/">Steve's Jobs</Link>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link className="nav-link text-white font-weight-bold" to="/dashboard">Dashboard</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link text-white font-weight-bold" to="/jobs">Jobs</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link text-white font-weight-bold" to="/login">Login</Link>
+          </li>
         </ul>
-      </li> */}
-      <li className="nav-item">
-        <Link className="nav-link active" to="/login">Login</Link>
-      </li>
-    </ul>
-    <form className="d-flex" role="search">
-      <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
-      <button className="btn btn-outline-success" type="submit">Search</button>
-    </form>
-  </div>
-</div>
-</nav>
+      </div>
+    </nav>
   )
 }
 
