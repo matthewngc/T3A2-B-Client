5335afe0cb3eea99c796888092c2c64e0ecf15fc
Timothy Nguyen <timhnguyen98@gmail.com>
2023-02-06 17:29:25 +1100

    Fixed vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import reactRefresh from '@vitejs/plugin-react-refresh'
 
 export default defineConfig({
-  plugins: [react(), reactRefresh()],
+  plugins: [react()],
   test: {
     environment: 'jsdom',
     globals: true
