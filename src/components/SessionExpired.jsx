1c4befa4c6cf6b6b33539deb8232cef13c882587
Timothy Nguyen <timhnguyen98@gmail.com>
2023-02-06 20:58:54 +1100

    added styling for SessionExpired page

--- src/components/SessionExpired.jsx
+++ src/components/SessionExpired.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './styles/SessionExpired.css'
 
 const SessionExpired = () => {
   return (
-    <div>
+    <div className='session-expired'>
         <h1>Your session has expired!</h1>
         <h2>Please <Link to='/login'>log in</Link> again.</h2>
     </div>
